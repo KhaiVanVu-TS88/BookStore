9ee88c6afdd758abc55810cb668b692d2f137c18
KhaiVanVu-TS88 <vuvankhai2022605100@gmail.com>
2025-06-04 20:06:24 +0700

    update cert with SAN

--- app.py
+++ app.py
@@ -248,4 +248,4 @@ def update_order():
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(ssl_context=('cert.pem', 'key.pem'), host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
